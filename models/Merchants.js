7ea75d132addcdd07a771a03963b1afc4f1a49dd
Damilare <dre4success@live.com>
2018-09-28 22:39:20 +0100

    setting up merchant schema

---
+++ models/Merchants.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const merchantSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: `Merchant name is required`
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [
+      {
+        type: Number,
+        required: `You must enter coordinates`
+      }
+    ],
+    address: {
+      type: String,
+      required: `You must provide an address`
+    }
+  },
+  city: {                                                   
+    type: String,
+    trim: true
+  },
+  state: {
+    type: String,
+    trim: true
+  }
+});
+
+merchantSchema.index({ location: '2dsphere' });
+
+module.exports = mongoose.model('Merchant', merchantSchema)
