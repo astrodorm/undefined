c03ca81fc3a28611a2219461b5964604b2e20560
Damilare <dre4success@live.com>
2018-09-28 22:39:42 +0100

    initializing a mongodb

---
+++ models/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  keepAlive: true
+};
+
+mongoose.connect(
+  process.env.MONGODB_URI,
+  options
+);
+mongoose.connection.on('error', err => {
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
+module.exports.Merchant = require('./Merchants');
