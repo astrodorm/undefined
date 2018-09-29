d8fb26de870e2fe0cab24c7d294dcb2cd498ca91
Damilare <dre4success@live.com>
2018-09-29 05:10:03 +0100

    loading product schema

--- models/index.js
+++ models/index.js
@@ -18,3 +18,4 @@ mongoose.connection.on('error', err => {
 module.exports.Merchant = require('./Merchants');
 module.exports.Customer = require('./Customers');
 module.exports.Shoppers = require('./Shoppers');
+module.exports.Product = require('./Product');
