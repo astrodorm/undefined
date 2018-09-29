9ab4e6ea22ff52382adf14e28a6646b7d2b40c1f
Damilare <dre4success@live.com>
2018-09-29 04:47:53 +0100

    Customer Schema

---
+++ models/Customers.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const bcrypt = require('bcrypt');
+
+const customerSchema = new mongoose.Schema({
+  fistname: {
+    type: String,
+    trim: true
+  },
+  lastname: {
+    type: String,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: `Email is required`,
+    trim: true
+  },
+  address: {
+    type: String,
+    trim: true
+  },
+  phoneNumber: {
+    type: Number,
+    trim: true
+  },
+  gender: {
+    type: String,
+    trim: true
+  },
+  birthday: String,
+  oauth: {
+    type: String,
+    required: `Password is required`
+  },
+  deviceType: String
+});
+
+customerSchema.pre('save', async function(next) {
+  let customer = this;
+  if (customer.isModified('oauth')) {
+    const hash = await bcrypt.hash(customer.oauth, 10);
+    customer.oauth = hash;
+    next();
+  } else {
+    next();
+  }
+});
+
+module.exports = mongoose.model('Customer', customerSchema);
