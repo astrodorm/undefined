fadcc94484a06157628304185c3fbc8b2d5e2740
Damilare <dre4success@live.com>
2018-09-29 15:13:35 +0100

    loading routes

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ require('dotenv').config({ path: 'variables.env' });
 const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
+const routes = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
+
+app.use('/api', routes);
 
 app.use((req, res, next) => {
   const err = new Error('No route Found');
