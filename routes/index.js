4dc72bea74ab3033feb9e302e36d4cebb301ffbd
Damilare <dre4success@live.com>
2018-09-29 16:57:12 +0100

    loading new routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const staff = require('../helpers/staff');
+const { auth } = require('../middleware/auth');
+const merchant = require('../helpers/merchants');
 
 router.route('/staff').post(staff.createStaff);
 router.route('/staff/login').post(staff.staffLogin);
+
+router.route('/merchants').post(auth, merchant.createMerchants);
 
 module.exports = router;
