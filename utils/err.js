482c665059af42dc8a9111b6caa28b4017390268
Damilare <dre4success@live.com>
2018-09-30 22:02:21 +0100

    reworking validation error

--- utils/err.js
+++ utils/err.js
@@ -2,12 +2,10 @@ exports.Emessage = (err, res) => {
   return res.status(400).json({ status: 400, message: err.message });
 };
 
-exports.Validator = (arr, req, res) => {
+exports.Validator = (arr, req) => {
   let errMe = [];
   for (let input of arr) {
     if (!req.body[input]) errMe.push(`${input} is required`);
   }
-
-  if (errMe.length >= 1)
-    return res.status(400).json({ status: 400, message: errMe });
+  return errMe;
 };
