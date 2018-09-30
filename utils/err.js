88dad1192fc66e853db5053452946772d4b1481c
Damilare <dre4success@live.com>
2018-09-30 08:45:27 +0100

    Validator function

--- utils/err.js
+++ utils/err.js
@@ -1,3 +1,13 @@
 exports.Emessage = (err, res) => {
   return res.status(400).json({ status: 400, message: err.message });
 };
+
+exports.Validator = (arr, req, res) => {
+  let errMe = [];
+  for (let input of arr) {
+    if (!req.body[input]) errMe.push(`${input} is required`);
+  }
+
+  if (errMe.length >= 1)
+    return res.status(400).json({ status: 400, message: errMe });
+};
