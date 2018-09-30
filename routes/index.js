a1045f1084d04caf97a75f909c590b22eae0527a
Damilare <dre4success@live.com>
2018-09-30 01:59:03 +0100

    loading routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const staff = require('../helpers/staff');
-const { auth } = require('../middleware/auth');
+const { auth, customerAuth } = require('../middleware/auth');
 const merchant = require('../helpers/merchants');
 const courier = require('../helpers/courier');
+const customer = require('../helpers/customer');
 
 router
   .route('/staff')
@@ -34,5 +35,16 @@ router
   .put(auth, courier.editCourier)
   .delete(auth, courier.deleteCourier)
   .get(auth, courier.FetchACourier);
+
+router
+  .route('/customers')
+  .post(customer.createCustomer)
+  .get(customerAuth, customer.currentlyLoggedInCustomer);
+
+router.route('/customers/login').post(customer.customerLogin);
+
+router
+  .route('/customers/admin')
+  .get(auth, customer.getAllCustomersForAdminUsage);
 
 module.exports = router;
