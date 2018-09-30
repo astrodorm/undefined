46b0998383948aeb29ff7511d76fc31a0c1b81fc
Damilare <dre4success@live.com>
2018-09-30 01:58:26 +0100

    customer endpoints

---
+++ helpers/customer.js
@@ -0,0 +1,82 @@
+const jwt = require('jsonwebtoken');
+const isEmail = require('validator/lib/isEmail');
+const db = require('../models');
+const { Emessage } = require('../utils/err');
+
+exports.createCustomer = async (req, res) => {
+  try {
+    let inputs = ['firstname', 'lastname', 'email', 'oauth'];
+    let err = [];
+    for (let input of inputs) {
+      if (!req.body[input]) err.push(`${input} is required`);
+    }
+    if (err.length >= 1)
+      return res.status(400).json({ status: 400, message: err });
+
+    if (req.body.phoneNumber && isNaN(Number(req.body.phoneNumber)))
+      return res
+        .status(400)
+        .json({ status: 400, message: `Phone  number must be numbers only` });
+
+    if (!isEmail(req.body.email))
+      return res
+        .status(400)
+        .json({ status: 400, message: `Please enter an email address` });
+
+    const exists = await db.Customer.findOne({ email: req.body.email });
+    if (exists)
+      return res
+        .status(400)
+        .json({ status: 400, message: `Customer with email already exist` });
+
+    const customer = await db.Customer.create(req.body);
+    const token = await jwt.sign({ _id: customer._id }, process.env.SECRET, {
+      expiresIn: '8760h'
+    });
+    res.status(200).json({ status: 200, data: { customer, token } });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.customerLogin = async (req, res) => {
+  try {
+    const customer = await db.Customer.findOne({ email: req.body.email });
+    if (!customer)
+      return res
+        .status(401)
+        .json({ status: 401, message: `Email or Password Incorrect` });
+
+    const pass = await customer.comparePassword(req.body.oauth);
+    if (!pass)
+      return res
+        .status(401)
+        .json({ status: 401, message: `Email or Password Incorrect` });
+
+    const token = await jwt.sign({ _id: customer._id }, process.env.SECRET, {
+      expiresIn: '8760h'
+    });
+    res.status(200).json({ status: 200, data: { customer, token } });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.currentlyLoggedInCustomer = async (req, res) => {
+  try {
+    return res
+      .status(200)
+      .json({ status: 200, data: { customer: req.customer } });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.getAllCustomersForAdminUsage = async (req, res) => {
+  try {
+    const customers = await db.Customer.find({});
+    res.status(200).json({ status: 200, data: customers });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
