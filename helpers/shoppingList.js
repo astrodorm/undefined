f614eed39508c53325328a6cccb95754d234bb1c
Damilare <dre4success@live.com>
2018-10-01 19:13:09 +0100

    removing from shoppingList

--- helpers/shoppingList.js
+++ helpers/shoppingList.js
@@ -1,5 +1,5 @@
 const db = require('../models');
-const {} = require('../');
+const { Emessage } = require('../utils/err');
 
 exports.addToCart = async (req, res) => {
   try {
@@ -10,13 +10,32 @@ exports.addToCart = async (req, res) => {
       customerCart = await db.ShoppingList.create({ customerID });
     }
 
+    if (req.body.productID) {
+      let shoppingCart = await db.ShoppingList.findOneAndUpdate(
+        {
+          _id: customerCart._id,
+          customerID
+        },
+        { $addToSet: { list: req.body.productID } },
+        { new: true }
+      ).populate('list', 'productName price thumbnail');
+      return res.status(200).json({ status: 200, data: shoppingCart });
+    }
+    return res.status(200).json({ status: 200, data: customerCart });
+  } catch (e) {
+    Emessage(e, req);
+  }
+};
+
+exports.removeFromCart = async (req, res) => {
+  try {
+    let customerID = req.customer._id;
+    let cart = await db.ShoppingList.findOne({ customerID });
     let shoppingCart = await db.ShoppingList.findOneAndUpdate(
-      {
-        customerID
-      },
-      { $addToSet: { list: req.body.productID } },
+      { _id: cart._id, customerID },
+      { $pull: { list: req.body.productID } },
       { new: true }
-    );
+    ).populate('list', 'productName price thumbnail');
 
     res.status(200).json({ status: 200, data: shoppingCart });
   } catch (e) {
