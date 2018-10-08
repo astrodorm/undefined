89d195fa5b009bc538cc033aa09c3e0eff58713e
Damilare <dre4success@live.com>
2018-10-08 16:43:28 +0100

    creating category

---
+++ helpers/category.js
@@ -0,0 +1,64 @@
+const db = require('../models');
+const { Emessage } = require('../utils/err');
+
+exports.createcategory = async (req, res) => {
+  try {
+    if (!req.body.categoryName)
+      return res
+        .status(400)
+        .json({ status: 400, message: `Category name is required` });
+
+    req.body.createdBy = req.admin.email;
+    const category = await db.Category.create(req.body);
+    res.status(200).json({ status: 200, data: category });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.fetchcategories = async (req, res) => {
+  try {
+    const categories = await db.Category.find({});
+    res.status(200).json({ status: 200, data: categories });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.editcategory = async (req, res) => {
+  try {
+    const category = await db.Category.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).json({ status: 200, data: category });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.deletecategory = async (req, res) => {
+  try {
+    const category = await db.Category.deleteOne({ _id: req.params.id });
+    if (category.ok && category.n)
+      return res
+        .status(200)
+        .json({ status: 200, data: `category successfully deleted` });
+
+    return res
+      .status(400)
+      .json({ status: 400, message: `Couldn't delete category` });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.fetchAcategory = async (req, res) => {
+  try {
+    const category = await db.Category.findOne({ _id: req.params.id });
+    res.status(200).json({ status: 200, data: category });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
