1b7a71f160c3f00e53942737894202b664f52b30
Damilare <dre4success@live.com>
2018-11-06 23:00:12 +0100

    order history schema

---
+++ models/OrderHistory.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const produceOrderSchema = new mongoose.Schema(
+  {
+    productName: { type: String, trim: true },
+    thumbnail: String,
+    price: Number,
+    merchantID: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Merchant'
+    },
+    customerID: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Customer'
+    },
+    quantity: {
+      type: Number,
+      default: 1
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('OrderHistory', produceOrderSchema);
