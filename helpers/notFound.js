bcf9db16741fa811867e2343ed8ca8fd4db55c38
Damilare <dre4success@live.com>
2018-11-27 18:15:40 +0100

    adding more methods on notfound

--- helpers/notFound.js
+++ helpers/notFound.js
@@ -3,6 +3,8 @@ const { Emessage } = require('../utils/err');
 
 exports.notFound = async (req, res) => {
   try {
+    req.body.email = req.customer.email;
+    req.body.phoneNumber = req.customer.phoneNumber;
     const notFound = await db.NotFound.create(req.body);
     res.status(200).json({ status: 200, data: notFound });
   } catch (e) {
@@ -14,6 +16,30 @@ exports.fetchNotFound = async (req, res) => {
   try {
     const notFounds = await db.NotFound.find({});
     res.status(200).json({ status: 200, data: notFounds });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.editNotFound = async (req, res) => {
+  try {
+    let _id = req.params.id;
+    let notFound = await db.NotFound.findOneAndUpdate(
+      { _id },
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json({ status: 200, data: notFound });
+  } catch (e) {
+    Emessage(e, res);
+  }
+};
+
+exports.getSingle = async (req, res) => {
+  try {
+    let _id = req.params.id;
+    let notFound = await db.NotFound.findOne({ _id });
+    res.status(200).json({ status: 200, data: notFound });
   } catch (e) {
     Emessage(e, res);
   }
