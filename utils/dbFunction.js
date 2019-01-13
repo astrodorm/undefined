44cebf4f4659c7ede9ff61dc2c30d148bba67834
Damilare <dre4success@live.com>
2019-01-13 20:51:39 +0100

    a resuable function

---
+++ utils/dbFunction.js
@@ -0,0 +1,42 @@
+const db = require('../models');
+
+exports.reusable = async stuff => {
+  let itemCode;
+  if (stuff.ITEMCODE) {
+    itemCode = stuff.ITEMCODE;
+  } else {
+    itemCode = stuff.itemCode;
+  }
+  const cataloguePromise = db.Catalogue.findOne({
+    itemCode
+  });
+
+  const productPromise = db.ProductV2.findOne({
+    itemCode
+  });
+  const [catalogue, product] = await Promise.all([
+    cataloguePromise,
+    productPromise
+  ]);
+
+  let thumbnail;
+  if (catalogue && catalogue.thumbnail) {
+    thumbnail = catalogue.thumbnail;
+  }
+  let isPickupAvailable, merchantID, categoryID;
+  if (product) {
+    isPickupAvailable = product.isPickupAvailable;
+    merchantID = product.merchantID;
+    categoryID = product.categoryID;
+  }
+  return {
+    ITEMCODE: itemCode,
+    DESCRIPTION: stuff.DESCRIPTION,
+    QUANTITY: stuff.QTY,
+    SELLINGPRICE: stuff.SELLINGPRICE,
+    image: thumbnail,
+    isPickupAvailable,
+    categoryID,
+    merchantID
+  };
+};
