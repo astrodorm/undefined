ea87b753dcdbaeaf3c972ee0689a96abf408f6a9
Damilare <dre4success@live.com>
2019-01-17 20:51:55 +0100

    shopping list schema

---
+++ models/ShoppingListV2.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const shoppingListSchema = new mongoose.Schema(
+  {
+    itemCode: { type: String, index: true },
+    customerID: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Customer',
+      index: true
+    },
+    productName: String,
+    quantity: {
+      type: Number,
+      default: 1
+    },
+    deliveryMethod: String,
+    convenienceFee: Number,
+    deliveryFee: Number,
+    total: Number,
+    sellingPrice: Number
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('ShoppingListV2', shoppingListSchema);
